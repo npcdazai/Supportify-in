e512b1be1b84b3992ef5ceffca9d1d2118b78f01
npcdazai <prathammandavkar932@gmail.com>
2025-01-26 18:34:58 +0530

    Banner

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
 import ArrowIcon from "../assets/icons/arrow-w.svg";
+import cursorImage from "../assets/images/cursor.png";
+import messageImage from "../assets/images/message.png";
 
 export const Hero = () => {
   return (
     <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px] sm:py-24 relative overflow-clip ">
-      <div className="absolute h-[375px] w-[750px] sm:w-[1536px] sm:h-[768px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)] "></div>
+      <div className="absolute h-[375px] w-[750px] sm:w-[1536px] sm:h-[768px] lg:w-[2400px] lg:h-[1200px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)] "></div>
       <div className="container relative ">
         <div className="flex items-center justify-center">
           <a
@@ -19,10 +22,26 @@ export const Hero = () => {
             </span>
           </a>
         </div>
-        <div className="flex justify-center ">
-          <h1 className="text-7xl sm:text-9xl font-bold tracking-tighter text-center mt-8 inline-flex ">
-            Where tech dreams <br /> hit the big screens
-          </h1>
+        <div className="flex justify-center mt-8 ">
+          <div className="inline-flex relative">
+            <h1 className="text-7xl sm:text-9xl font-bold tracking-tighter text-center  inline-flex ">
+              Where tech dreams <br /> hit the big screens
+            </h1>
+            <Image
+              alt="msgIcon"
+              src={messageImage}
+              height="200"
+              width="200"
+              className="absolute right-[-100px] top-[200px] hidden sm:inline "
+            />
+            <Image
+              alt="cursorIcon"
+              src={cursorImage}
+              height="200"
+              width="200"
+              className="absolute top-[220px] left-[-70px] hidden sm:inline  "
+            />
+          </div>
         </div>
         <div className="flex justify-center">
           <p className="text-center text-xl mt-8 max-w-md">
