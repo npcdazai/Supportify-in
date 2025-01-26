85c7de9956ff81c61b3d3bc8e2b5a4afa62fae85
npcdazai <prathammandavkar932@gmail.com>
2025-01-26 17:58:37 +0530

    Banner and Hero

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,17 @@
 export const Banner = () => {
-  return null;
+  return (
+    <div className="py-3 text-center bg-[linear-gradient(to_right,#FCD6FF,#29D8FF,#FFFD80,#F89ABF,#FCD6FF)]">
+      <div className="container">
+        <p className="font-medium" >
+          <span className="hidden sm:inline" >
+            Introducing a company where Quality reigns supreme—we deliver the
+            best! -{" "}
+          </span>
+          <a href="#" className="underline underline-offset-4">
+            Explore our Starter Pack
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
